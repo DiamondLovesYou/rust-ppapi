83a1a24eef6d416fff12996f0e529d300ed81365
Richard Diamond <wichard@vitalitystudios.com>
2014-08-11 12:09:51 -0500

    Update docs.

--- doc-gen.sh
+++ doc-gen.sh
@@ -8,9 +8,12 @@ DOCS_DIR="`readlink -f docs`"
 export LD_LIBRARY_PATH=$SYSROOT/lib:$LD_LIBRARY_PATH
 
 mkdir -p $BUILD_DIR
-rm -fr $DOCS_DIR/*
-  cd $1 &&
+git rm -fr $DOCS_DIR
+mkdir -p $DOCS_DIR
+cd $1 &&
 # We strictly speaking don't need to build libressl, but meh.
   remake SYSROOT="$SYSROOT" NACL_SDK="$NACL_SDK" BUILD_DIR="$BUILD_DIR" &&
   rustdoc -L $BUILD_DIR --target=le32-unknown-nacl -o $DOCS_DIR src/lib.rs
 
+cd $4 &&
+  git add docs
