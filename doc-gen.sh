bc36b4277df18fa89ad6ee9e20dada7a496f45ca
Richard Diamond <wichard@vitalitystudios.com>
2014-08-11 11:48:05 -0500

    Update docs.

--- doc-gen.sh
+++ doc-gen.sh
@@ -1,21 +1,16 @@
 #/bin/sh
 
-SYSROOT="`readlink -f $1`"
-NACL_SDK="`readlink -f $2`"
+SYSROOT="`readlink -f $2`"
+NACL_SDK="`readlink -f $3`"
 BUILD_DIR="`readlink -f build`"
 DOCS_DIR="`readlink -f docs`"
 
 export LD_LIBRARY_PATH=$SYSROOT/lib:$LD_LIBRARY_PATH
 
 mkdir -p $BUILD_DIR
-rm -fr tmp_src
 rm -fr $DOCS_DIR/*
-git clone https://github.com/DiamondLovesYou/rust-ppapi.git tmp_src &&
-  cd tmp_src &&
-  git submodule update --init &&
+  cd $1 &&
 # We strictly speaking don't need to build libressl, but meh.
-  remake clean &&
   remake SYSROOT="$SYSROOT" NACL_SDK="$NACL_SDK" BUILD_DIR="$BUILD_DIR" &&
-  rustdoc -L $BUILD_DIR --target=le32-unknown-nacl -o $DOCS_DIR lib.rs
+  rustdoc -L $BUILD_DIR --target=le32-unknown-nacl -o $DOCS_DIR src/lib.rs
 
-rm -fr tmp_src
