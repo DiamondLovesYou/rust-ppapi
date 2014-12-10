ccd8dd704a2910bfdd60d527654521177141bd01
Richard Diamond <wichard@vitalitystudios.com>
2014-12-09 23:13:36 -0600

    Move from LGPL-3 to MPL-2.0.

--- src/lib/input.rs
+++ src/lib/input.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2014 Richard Diamond & contributors.
+//
+// This file is part of the Rust PPApi project.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use super::{Resource, ResourceType, Ticks, FloatPoint,
             StringVar, Point, TouchPoint};
 use super::{ppb, ffi};
--- src/lib/ppp.rs
+++ src/lib/ppp.rs
@@ -2,18 +2,9 @@
 //
 // This file is part of the Rust PPApi project.
 //
-// Rust PPApi is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Rust PPApi is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with Rust PPApi. If not, see <http://www.gnu.org/licenses/>.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use std::c_str;
 use std::ptr;
